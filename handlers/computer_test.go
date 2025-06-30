b17f4ccb1a8455048f1dd7e6662343c421b1a0a1
Ahmed Magdy <ahmadmgdi94@gmail.com>
2025-06-30 11:12:15 +0200

    Implemented Unassign Endpoint

--- handlers/computer_test.go
+++ handlers/computer_test.go
@@ -27,6 +27,7 @@ func setupTestApp() *fiber.App {
 	app.Put("/api/computers/:id", UpdateComputerByID)
 	app.Put("/api/computers/:id/assign/:abbr", AssignComputer)
 	app.Get("/api/employees/:abbr/computers", GetEmployeeComputers)
+	app.Post("/api/computers/:id/unassign", UnAssignComputer)
 	return app
 }
 
@@ -357,6 +358,32 @@ func TestGetEmployeeComputerError(t *testing.T) {
 
 	body, _ := io.ReadAll(resp.Body)
 	assert.Equal(t, `{"error":"failed to fetch employee computers"}`, string(body))
+}
+
+func TestUnAssignComputer(t *testing.T) {
+	app := setupTestApp()
+	computer := createComputer("TestPC", "11:22:33:44:55:90", "192.168.1.2", "EMP1")
+	req := httptest.NewRequest("POST", fmt.Sprintf("/api/computers/%d/unassign", computer.ID), nil)
+	resp, err := app.Test(req)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
+	var updatedComputer models.Computer
+	body, _ := io.ReadAll(resp.Body)
+	err = json.Unmarshal(body, &updatedComputer)
+	assert.Nil(t, err)
+	assert.Equal(t, "", updatedComputer.EmployeeAbbreviation)
+}
+
+func TestUnAssignComputerNotFound(t *testing.T) {
+	app := setupTestApp()
+	fakeID := 9999
+	req := httptest.NewRequest("POST", fmt.Sprintf("/api/computers/%d/unassign", fakeID), nil)
+	resp, err := app.Test(req)
+	assert.Nil(t, err)
+	assert.Equal(t, 404, resp.StatusCode)
+
+	body, _ := io.ReadAll(resp.Body)
+	assert.Equal(t, `{"error":"Can't find Computer with ID 9999"}`, string(body))
 }
 
 func createComputer(name, macAddress, ipAddr, employeeAbbreviation string) models.Computer {
